0429fb019c5114db85981156b0d8efe4439a1aba
mdubois <maxou.dub76@gmail.com>
2024-09-19 17:12:31 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Utilise 'dist' comme répertoire de sortie
-  },
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Utilise 'dist' comme répertoire de sortie
+        external: ['html5-qrcode'],
+    },
 });
