be6f30286f66234c1509b4d76098f2bc644ad1e8
mdubois <maxou.dub76@gmail.com>
2024-09-19 20:00:58 +0200

    :construction: add front for new route search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import React from 'react';
 import BarcodeScanner from './BarcodeScanner';
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <h1>Scanner de Code-Barres</h1>
       <BarcodeScanner />
+      <SearchBar />
     </div>
   );
 }
